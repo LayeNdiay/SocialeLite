85cce22640679e13fc9b9e8efe2e0c35c3818cb7
AzPrime <abdoulazizebe44@gmail.com>
2023-07-01 15:31:23 +0000

    discussion working 60%

--- Views/discussion.php
+++ Views/discussion.php
@@ -1,17 +1,97 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-   <meta charset="utf-8">
-   <!-- importer le fichier de style -->
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+   <script
+      src="https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js?lang=xml&amp;skin=sunburst"></script>
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+      crossorigin="anonymous"></script>
+   <link rel="stylesheet" href="./src/style.css" />
+   <title>Document</title>
+
 </head>
 
 <body>
-   <div id="container">
-      <h1>
-         <?php echo isset($_GET['discussion_id']) ? $_GET['discussion_id'] : ''; ?>
-      </h1>
+   <div class="contenant">
+      <div style="display: flex">
+         <div class="codeXML">
+            <pre class="prettyprint">
+             <code class="language-xml">
+             <?php
+             highlight_file("../DAO/data.xml"); ?>   
+            </code>
+            </pre>
+         </div>
+         <div class="formulaire">
+
+            <section class="msger">
+               <header class="msger-header">
+                  <div class="msger-header-title">
+                     <span class="material-symbols-outlined">
+                        <i class="fa fa-chevron-left" style="font-size:24px"></i>
+                     </span> SimpleChat
+                  </div>
+               </header>
+               <main class="msger-chat">
+                  <div class="msg left-msg">
+                     <div class="msg-bubble">
+                        <div class="msg-info">
+                           <div class="msg-info-name">BOT</div>
+                           <div class="msg-info-time">12:45
+                              <div class="btn-group">
+                                 <!-- <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown"
+                                    aria-expanded="false">
+                                    Action
+                                 </button> -->
+                                 <button class="chevron-down dropdown-toggle" data-bs-toggle="dropdown"></button>
+                                 <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Citer ce message</a></li>
+
+                                 </ul>
+                              </div>
+
+
+                           </div>
+                        </div>
+
+                        <div class="msg-text">
+                           Hi, welcome to Simple 😄
+                        </div>
+                     </div>
+                  </div>
+
+                  <div class="msg right-msg">
+                     <div class="msg-bubble">
+                        <div class="msg-info">
+                           <div class="msg-info-name">Sajad</div>
+                           <div class="msg-info-time">12:46</div>
+                        </div>
+                        <div class="msg-text">You can change your name in JS section!</div>
+                     </div>
+                  </div>
+               </main>
+
+               <form class="msger-inputarea">
+                  <input type="text" class="msger-input" placeholder="Votre message." />
+                  <button type="submit" class="msger-send-btn">envoyer
+                  </button>
+               </form>
+            </section>
+         </div>
+      </div>
    </div>
 </body>
 
 </html>
+<style>
+   <?php include $_SERVER['DOCUMENT_ROOT'] . "SocialeLite/src/style.css"; ?>
+</style>
+<script>
+   <?php include $_SERVER['DOCUMENT_ROOT'] . "SocialeLite/src/script.js"; ?>
+
+</script>
---
+++ src/script.js
@@ -0,0 +1,46 @@
+const msgerForm = get('.msger-inputarea');
+const msgerInput = get('.msger-input');
+const msgerChat = get('.msger-chat');
+
+msgerForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const msgText = msgerInput.value;
+  if (!msgText) return;
+
+  appendMessage('right', msgText);
+  msgerInput.value = '';
+});
+
+function appendMessage(side, text) {
+  //   Simple solution for small apps
+  const msgHTML = `
+    <div class="msg ${side}-msg">
+      <div class="msg-bubble">
+        <div class="msg-info">
+          <div class="msg-info-time">${formatDate(new Date())}</div>
+        </div>
+        <div class="msg-text">${text}</div>
+      </div>
+    </div>
+  `;
+
+  msgerChat.insertAdjacentHTML('beforeend', msgHTML);
+  msgerChat.scrollTop += 500;
+}
+
+// Utils
+function get(selector, root = document) {
+  return root.querySelector(selector);
+}
+
+function formatDate(date) {
+  const h = '0' + date.getHours();
+  const m = '0' + date.getMinutes();
+
+  return `${h.slice(-2)}:${m.slice(-2)}`;
+}
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
