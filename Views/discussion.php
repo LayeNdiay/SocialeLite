b2519d1598d66fe4ed345e4ba7dc8f29807e3e30
AzPrime <abdoulazizebe44@gmail.com>
2023-07-07 18:18:02 +0000

    major changes | discussion working 98% | error when cree and send a msg right after

--- Views/discussion.php
+++ Views/discussion.php
@@ -7,11 +7,11 @@
    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
    <script
       src="https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js?lang=xml&amp;skin=sunburst">
-      </script>
+   </script>
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
-      </script>
+   </script>
    <link rel="stylesheet" href="./src/style.css" />
    <title>Document</title>
    <script src="https://unpkg.com/wavesurfer.js"></script>
@@ -30,7 +30,7 @@ $messages = $discusions["messages"];
             <pre class="prettyprint">
              <code class="language-xml">
                <?php
-               //  highlight_file(dirname(__DIR__) . DIRECTORY_SEPARATOR . "DAO" . DIRECTORY_SEPARATOR . "data.xml"); ?>   
+               highlight_file(dirname(__DIR__) . DIRECTORY_SEPARATOR . "DAO" . DIRECTORY_SEPARATOR . "data.xml"); ?>   
             </code>
             </pre>
          </div>
@@ -49,85 +49,81 @@ $messages = $discusions["messages"];
                      $sender = $message->getExpediteur();
                      $time = $message->getCreatedAt();
                      if ($sender->getId() == $id_interlocuteur) { ?>
-                        <div class="msg left-msg">
-                           <div class="msg-bubble">
-                              <div class="msg-info">
-                                 <div class="msg-info-name">
-                                    <?= $sender->getName() ?>
-                                 </div>
-                                 <div class="msg-info-time">
-                                    <?= $time->format("H:i") ?>
-                                    <div class="btn-group"> <button class="chevron-down dropdown-toggle"
-                                          data-bs-toggle="dropdown"></button>
-                                       <ul class="dropdown-menu">
-                                          <li><a class="dropdown-item" href="#">Citer ce message</a></li>
-                                       </ul>
-                                    </div>
-                                 </div>
+                  <div class="msg left-msg">
+                     <div class="msg-bubble">
+                        <div class="msg-info">
+                           <div class="msg-info-name">
+                              <?= $sender->getName() ?>
+                           </div>
+                           <div class="msg-info-time">
+                              <?= $time->format("H:i") ?>
+                              <div class="btn-group"> <button class="chevron-down dropdown-toggle"
+                                    data-bs-toggle="dropdown"></button>
+                                 <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Citer ce message</a></li>
+                                 </ul>
                               </div>
-                              <?php if ($message->getType() == "texte") { ?>
-                                 <div class="msg-text">
-                                    <!-- <?= $message->getContent() ?> -->
-                                    <pre>
+                           </div>
+                        </div>
+                        <?php if ($message->getType() == "texte") { ?>
+                        <div class="msg-text">
+                           <?= $message->getContent() ?>
 
-                                                         <?php var_dump($discusions); ?>
-                                                      </pre>
-                                 </div>
-                                 <?php
+                        </div>
+                        <?php
                               } else { ?>
-                                 <div class="msg-text">
-                                    <div id="waveform"></div>
-                                    <?php require_once $this->viewsPath . "/asset/button.php"; ?>
-                                 </div>
-                              <?php } ?>
-                           </div>
+                        <div class="msg-text">
+                           <div id="waveform"></div>
+                           <?php require_once $this->viewsPath . "/asset/button.php"; ?>
                         </div>
-                     <?php } else { ?>
-                        <div class="msg right-msg">
-                           <div class="msg-bubble">
-                              <div class="msg-info">
-                                 <div class="msg-info-name">Vous</div>
-                                 <div class="msg-info-time">
-                                    <?= $time->format("H:i") ?>
-                                 </div>
-                              </div>
-                              <?php if ($message->getType() == "texte") { ?>
-                                 <div class="msg-text">
-                                    <?= $message->getContent() ?>
-                                 </div>
-                              <?php } else { ?>
-
-                                 <div class="msg-text">
-                                    <div id="waveform"></div>
-                                    <?php require_once $this->viewsPath . "/asset/button.php"; ?>
-                                 </div>
-                              <?php } ?>
-                           </div>
-                        </div>
-                     <?php }
-                  } ?>
-               </main>
-               <form class="msger-inputarea" enctype="multipart/form-data" action=<?= "/SocialeLite/messages/create/" . $idDiscussion . "/$id_interlocuteur" ?> method="POST">
-                  <div class="image-upload">
-                     <label for="file-input">
-                        <i class="fa fa-microphone"></i> </label>
-                     <input name="audio" id="file-input" type="file" accept=".mp3,audio/*" />
+                        <?php } ?>
+                     </div>
                   </div>
-                  <input name="text" type="text" class="msger-input" placeholder="Votre message." />
-                  <button type="submit" class="msger-send-btn">envoyer
-                  </button>
-               </form>
-            </section>
+                  <?php } else { ?>
+                  <div class="msg right-msg">
+                     <div class="msg-bubble">
+                        <div class="msg-info">
+                           <div class="msg-info-name">Vous</div>
+                           <div class="msg-info-time">
+                              <?= $time->format("H:i") ?>
+                           </div>
+                        </div>
+                        <?php if ($message->getType() == "texte") { ?>
+                        <div class="msg-text">
+                           <?= $message->getContent() ?>
+                        </div>
+                        <?php } else { ?>
+
+                        <div class="msg-text">
+                           <div id="waveform"></div>
+                        </div>
+                        <?php require_once $this->viewsPath . "/asset/button.php"; ?>
+                     </div>
+                     <?php } ?>
+                  </div>
          </div>
+         <?php }
+                  } ?>
+         </main>
+         <form class="msger-inputarea" enctype="multipart/form-data"
+            action=<?= "/SocialeLite/messages/create/" . $idDiscussion . "/" . $id_interlocuteur ?> method="POST">
+            <div class="image-upload">
+               <label for="file-input">
+                  <i class="fa fa-microphone"></i> </label>
+               <input name="audio" id="file-input" type="file" accept=".mp3,audio/*" />
+            </div>
+            <input name="text" type="text" class="msger-input" placeholder="Votre message." />
+            <button type="submit" class="msger-send-btn">envoyer
+            </button>
+         </form>
+         </section>
       </div>
+   </div>
    </div>
 </body>
 
 </html>
 <style>
-   <?php include $_SERVER['DOCUMENT_ROOT'] . "SocialeLite/src/style.css";
-   ?>
+<?php include $_SERVER['DOCUMENT_ROOT'] . "SocialeLite/src/style.css";
+?>
 </style>
-<script>
-   <?php include $_SERVER['DOCUMENT_ROOT'] . "SocialeLite/src/script.js"; ?>
-</script>
--- Views/discussionGroup.php
+++ Views/discussionGroup.php
@@ -7,11 +7,11 @@
    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
    <script
       src="https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js?lang=xml&amp;skin=sunburst">
-   </script>
+      </script>
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
-   </script>
+      </script>
    <script src="https://unpkg.com/wavesurfer.js"></script>
 
    <link rel="stylesheet" href="./src/style.css" />
@@ -22,6 +22,7 @@
    <?php
    $groupInfo = $discusions["groupe"];
    $messages = $discusions["messages"];
+   $idDiscussion = $groupInfo->idDiscussion;
    $users = $groupInfo->contacts;
    $strangers = [];
    foreach ($tableauContacts as $contact) {
@@ -66,9 +67,9 @@
                               <hr class="dropdown-divider">
                            </li>
                            <?php foreach ($users as $user) { ?>
-                           <li><a class="dropdown-item" href="#">
-                                 <?php echo $user->getId() == $user_id ? "Vous" : $user->getName() ?>
-                              </a></li>
+                              <li><a class="dropdown-item" href="#">
+                                    <?php echo $user->getId() == $user_id ? "Vous" : $user->getName() ?>
+                                 </a></li>
                            <?php } ?>
                         </ul>
                      </div>
@@ -76,13 +77,12 @@
                         <button type="button" class="menu dropdown-toggle" data-bs-toggle="dropdown">
                            <i class="fa fa-plus"></i>
                         </button>
-                        <form class="dropdown-menu p-4"
-                           action=<?= "/SocialeLite/groupes/membre/" . $groupInfo->getId() ?> method="POST">
+                        <form class="dropdown-menu p-4" action=<?= "/SocialeLite/groupes/membre/" . $groupInfo->getId() ?> method="POST">
                            <select name="contact" class="form-select" aria-label="Default select example">
                               <?php foreach ($strangers as $strang_user) {
                                  ?>
-                              <option value=<?= $strang_user->getId() ?>><?= $strang_user->getName() ?>
-                              </option>
+                                 <option value=<?= $strang_user->getId() ?>><?= $strang_user->getName() ?>
+                                 </option>
                               <?php } ?>
                            </select>
                            <button type="submit" class="btn btn-primary mt-3">Ajouter</button>
@@ -95,69 +95,69 @@
                      $sender = $message->getExpediteur();
                      $time = $message->getCreatedAt();
                      if ($sender->getId() != $user_id) { ?>
-                  <div class="msg left-msg">
-                     <div class="msg-bubble">
-                        <div class="msg-info">
-                           <div class="msg-info-name">
-                              <?= $sender->getName() ?>
-                           </div>
-                           <div class="msg-info-time">
-                              <?= $time->format("H:i") ?>
-                              <div class="btn-group">
-                                 <button class="chevron-down dropdown-toggle" data-bs-toggle="dropdown">
-                                    <i class="fa fa fa-chevron-down"></i>
-                                 </button>
-                                 <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Citer ce message</a></li>
-                                 </ul>
+                        <div class="msg left-msg">
+                           <div class="msg-bubble">
+                              <div class="msg-info">
+                                 <div class="msg-info-name">
+                                    <?= $sender->getName() ?>
+                                 </div>
+                                 <div class="msg-info-time">
+                                    <?= $time->format("H:i") ?>
+                                    <div class="btn-group">
+                                       <button class="chevron-down dropdown-toggle" data-bs-toggle="dropdown">
+                                          <i class="fa fa fa-chevron-down"></i>
+                                       </button>
+                                       <ul class="dropdown-menu">
+                                          <li><a class="dropdown-item" href="#">Citer ce message</a></li>
+                                       </ul>
+                                    </div>
+                                 </div>
                               </div>
-                           </div>
-                        </div>
-                        <?php if ($message->getType() == "texte") { ?>
-                        <div class="msg-text">
-                           <?= $message->getContent() ?>
-                        </div>
-                        <?php
+                              <?php if ($message->getType() == "texte") { ?>
+                                 <div class="msg-text">
+                                    <?= $message->getContent() ?>
+                                 </div>
+                                 <?php
                               } else { ?>
-                        <div class="msg-text">
-                           <div id="waveform"></div>
-                           <?php require_once $this->viewsPath . "/asset/button.php"; ?>
-                        </div>
-                        <?php } ?>
-                     </div>
-                  </div>
-                  <?php } else { ?>
-                  <div class="msg right-msg">
-                     <div class="msg-bubble">
-                        <div class="msg-info">
-                           <div class="msg-info-name">Vous</div>
-                           <div class="msg-info-time">
-                              <?= $time->format("H:i") ?>
+                                 <div class="msg-text">
+                                    <div id="waveform"></div>
+                                    <?php require_once $this->viewsPath . "/asset/button.php"; ?>
+                                 </div>
+                              <?php } ?>
                            </div>
                         </div>
-                        <?php if ($message->getType() == "texte") { ?>
-                        <div class="msg-text">
-                           <?= $message->getContent() ?>
-                        </div>
-                        <?php } else { ?>
+                     <?php } else { ?>
+                        <div class="msg right-msg">
+                           <div class="msg-bubble">
+                              <div class="msg-info">
+                                 <div class="msg-info-name">Vous</div>
+                                 <div class="msg-info-time">
+                                    <?= $time->format("H:i") ?>
+                                 </div>
+                              </div>
+                              <?php if ($message->getType() == "texte") { ?>
+                                 <div class="msg-text">
+                                    <?= $message->getContent() ?>
+                                 </div>
+                              <?php } else { ?>
 
-                        <div class="msg-text">
-                           <div id="waveform"></div>
-                           <?php require_once $this->viewsPath . "/asset/button.php"; ?>
+                                 <div class="msg-text">
+                                    <div id="waveform"></div>
+                                    <?php require_once $this->viewsPath . "/asset/button.php"; ?>
+                                 </div>
+                              <?php } ?>
+                           </div>
                         </div>
-                        <?php } ?>
-                     </div>
-                  </div>
-                  <?php }
+                     <?php }
                   } ?>
                </main>
-               <form class="msger-inputarea">
+               <form class="msger-inputarea" enctype="multipart/form-data" action=<?= "/SocialeLite/messages/create/" . $idDiscussion . "/" . $user_id ?> method="POST">
                   <div class="image-upload">
                      <label for="file-input">
                         <i class="fa fa-microphone"></i> </label>
-                     <input id="file-input" type="file" accept=".mp3,audio/*" />
+                     <input name="audio" id="file-input" type="file" accept=".mp3,audio/*" />
                   </div>
-                  <input type="text" class="msger-input" placeholder="Votre message." />
+                  <input name="text" type="text" class="msger-input" placeholder="Votre message." />
                   <button type="submit" class="msger-send-btn">envoyer
                   </button>
                </form>
@@ -169,12 +169,13 @@
 
 </html>
 <style>
-<?php include $_SERVER['DOCUMENT_ROOT'] . "SocialeLite/src/style.css";
+   <?php include $_SERVER['DOCUMENT_ROOT'] . "SocialeLite/src/style.css";
 
-?>.dropdown-toggle::after {
-   display: none;
-}
+   ?>
+   .dropdown-toggle::after {
+      display: none;
+   }
 </style>
 <script>
-<?php include $_SERVER['DOCUMENT_ROOT'] . "SocialeLite/src/script.js"; ?>
+   <?php include $_SERVER['DOCUMENT_ROOT'] . "SocialeLite/src/script.js"; ?>
 </script>
